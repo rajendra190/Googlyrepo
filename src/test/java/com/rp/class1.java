ab3b3886b50f5d42483793f91cf9902e1bba38b4
rajendra190 <rajendraprasadmanchineella190@gmail.com>
2022-03-08 19:18:46 +0530

    second commit for the day

---
+++ src/test/java/com/rp/class1.java
@@ -0,0 +1,10 @@
+package com.rp;
+
+public class class1 {
+
+	public static void main(String[] args) {
+		System.out.println("jhakshd");
+
+	}
+
+}
---
+++ src/test/java/com/rp/class2.java
@@ -0,0 +1,10 @@
+package com.rp;
+
+public class class2 {
+
+	public static void main(String[] args) {
+		System.out.println("shdhkhf");
+
+	}
+
+}
